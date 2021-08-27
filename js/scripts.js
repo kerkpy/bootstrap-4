2e1b821f00ab196e9643ffefd0353bbcab2801c1
kerkpy <peiyongkerk@gmail.com>
2021-08-27 18:13:28 +0800

    NPM Scripts

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    })
+
+    $("#loginbutton").click(function() {
+        $('#loginModal').modal('show');
+
+    })
+
+    $("#resbutton").click(function() {
+        $('#reserveModal').modal('show');
+
+    }
+    )
+});
